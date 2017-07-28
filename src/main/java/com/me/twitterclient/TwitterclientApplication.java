7f9b0b1c7046a5fa47df99c6d2b66065ed73f991
vinaycn <vinaykumarcn25@gmail.com>
2017-07-28 01:58:10 -0400

    renamed pacake name

--- src/main/java/com/me/twitterclient/TwitterclientApplication.java
+++ src/main/java/com/me/twitterclient/TwitterclientApplication.java
@@ -1,4 +1,4 @@
-package com.vinay.twitterclient;
+package com.me.twitterclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/me/twitterclient/controller/IndexController.java
+++ src/main/java/com/me/twitterclient/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.vinay.twitterclient.controller;
+package com.me.twitterclient.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
